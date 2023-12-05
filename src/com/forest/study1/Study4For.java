fa0dbeb0e81fbb7560f458668ea6a23b43f4cc3d
SeokHoon <shheo90oi@gmail.com>
2023-12-05 15:07:50 +0900

    add

--- src/com/forest/study1/Study4For.java
+++ src/com/forest/study1/Study4For.java
@@ -1,5 +1,7 @@
 package com.forest.study1;
 
+import java.util.Scanner;
+
 public class Study4For {
 	public static void main(String [] args) {
 //		서든 FPS
@@ -8,5 +10,26 @@ public class Study4For {
 //		1. 단발모드
 //		2. 점사모드
 		
+		String oneShot = "탕";
+		String ThreeShot = "탕탕탕";
+		
+		Scanner sc = new Scanner(System.in);
+		for(int Tan = 1 ; Tan <= 3 ; Tan++) {
+			int mode = 0;
+			System.out.println("발사모드를 선택하세요 : 1=단발 / 2=점사");
+			mode = sc.nextInt();
+			if(mode == 1) {
+				for(int i = 0; i <= 30 ; i++) {
+					System.out.println(oneShot +i);
+				}
+			}else if(mode == 2) {
+				for(int j = 0; j <= 30 ; j+=3) {
+					System.out.println(ThreeShot + j);
+				}
+			}else {
+				Tan--;
+				}
+			}
+		sc.close();
 	}
 }
